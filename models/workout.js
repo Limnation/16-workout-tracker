d3f01685b00654fd19aa6e90d699aaba166631d7
Scott Erwin <scottallenerwin@gmail.com>
2021-06-19 20:15:21 -0400

    added the workout schema to workout.js

--- models/workout.js
+++ models/workout.js
@@ -1,7 +1,47 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const workoutSchema = new Schema({});
+const workoutSchema = new Schema({
+  date: {
+    type: Date,
+    default: Date.now,
+  },
+  exercises: [
+    {
+      name: {
+        type: String,
+        trim: true,
+        required: "Enter the exercises' name",
+      },
+      type: {
+        type: String,
+        trim: true,
+        required: "Enter the exercises' type",
+      },
+      weight: {
+        type: Number,
+        default: 0,
+      },
+      sets: {
+        type: Number,
+        default: 0,
+      },
+      reps: {
+        type: Number,
+        default: 0,
+      },
+      duration: {
+        type: Number,
+        required: "Enter duration in min.",
+        default: 0,
+      },
+      distance: {
+        type: Number,
+        default: 0,
+      },
+    },
+  ],
+});
 
 const workout = mongoose.model("Workout", workoutSchema);
 
