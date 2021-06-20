36ce6bac5a03a405d1f6645fe1fe88f498754088
Scott Erwin <scottallenerwin@gmail.com>
2021-06-19 21:36:55 -0400

    added the put and get for workouts range and by id

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -11,6 +11,31 @@ router.post("/api/workouts", ({ body }, res) => {
     });
 });
 
+router.put("/api/workouts/:id", ({ params, body }, res) => {
+  Workout.findOneAndUpdate(
+    { _id: params.id },
+    { $push: { exercises: body } },
+    { new: true }
+  )
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get("/api/workouts/range", (req, res) => {
+  Workout.find({})
+    .limit(7)
+    .then((dbWorkout) => {
+      res.json(dbWorkout);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
 router.get("/api/workouts", (req, res) => {
   Workout.find({})
     .then((dbWorkout) => {
