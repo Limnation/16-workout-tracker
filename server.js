18fcb35d8c3f4b68f97dc7ee95352835178ab183
Scott Erwin <scottallenerwin@gmail.com>
2021-06-22 21:10:29 -0400

    updated server.js

--- server.js
+++ server.js
@@ -10,6 +10,7 @@ app.use(morgan("dev"));
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
 // app.use(express.static(path.join(__dirname, "public")));
 
 mongoose.connect(
